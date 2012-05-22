333753da39e79971e26b3a178df05181573ce854
Oliver Caldwell <olliec87@gmail.com>
2012-05-22 23:04:02 +0100

    Added tests for the contains module.

---
+++ tests/spec/contains.js
@@ -0,0 +1,96 @@
+require(['photon/contains'], function(contains) {
+    describe('contains', function() {
+        it('should find a string in an array that is present', function() {
+            var a = ['foo', 'bar', 'baz'];
+            expect(contains(a, 'foo')).toEqual(true);
+        });
+        
+        it('should not find a string in an array that is not present', function() {
+            var a = ['foo', 'bar', 'baz'];
+            expect(contains(a, 'gone')).toEqual(false);
+        });
+        
+        it('should find a string in an object that is present', function() {
+            var a = {
+                'one': 'foo',
+                'two': 'bar',
+                'three': 'baz'
+            };
+            expect(contains(a, 'foo')).toEqual(true);
+        });
+        
+        it('should not find a string in an object that is not present', function() {
+            var a = {
+                'one': 'foo',
+                'two': 'bar',
+                'three': 'baz'
+            };
+            expect(contains(a, 'gone')).toEqual(false);
+        });
+        
+        it('should find a array in an array that is present', function() {
+            var b = ['one', 'two'];
+            var a = ['foo', 'bar', b];
+            expect(contains(a, b)).toEqual(true);
+        });
+        
+        it('should not find a array in an array that is not present', function() {
+            var c = ['three', 'four'];
+            var b = ['one', 'two'];
+            var a = ['foo', 'bar', b];
+            expect(contains(a, c)).toEqual(false);
+        });
+        
+        it('should find a object in an array that is present', function() {
+            var b = {
+                'a': 'one',
+                'b': 'two'
+            };
+            var a = ['foo', 'bar', b];
+            expect(contains(a, b)).toEqual(true);
+        });
+        
+        it('should not find a object in an array that is not present', function() {
+            var c = {
+                'c': 'three',
+                'd': 'four'
+            };
+            var b = {
+                'a': 'one',
+                'b': 'two'
+            };
+            var a = ['foo', 'bar', b];
+            expect(contains(a, c)).toEqual(false);
+        });
+        
+        it('should find a object in an object that is present', function() {
+            var b = {
+                'a': 'one',
+                'b': 'two'
+            };
+            var a = {
+                'x': 'foo',
+                'y': 'bar',
+                'z': b
+            };
+            expect(contains(a, b)).toEqual(true);
+        });
+        
+        it('should not find a object in an object that is not present', function() {
+            var c = {
+                'c': 'three',
+                'd': 'four'
+            };
+            var b = {
+                'a': 'one',
+                'b': 'two'
+            };
+            var a = {
+                'x': 'foo',
+                'y': 'bar',
+                'z': b
+            };
+            expect(contains(a, c)).toEqual(false);
+        });
+    });
+});
