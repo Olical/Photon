d810f57e2a65885284f3b74fa1fccd417941e3e4
Oliver Caldwell <olliec87@gmail.com>
2012-05-22 23:18:28 +0100

    Added tests for each.

---
+++ tests/spec/each.js
@@ -0,0 +1,49 @@
+require(['photon/each'], function(each) {
+    describe('each', function() {
+        it('should pass all values from an array', function() {
+            var b = ['a', 'b', 'c'];
+            var a = '';
+            each(b, function(value) {
+                a += value;
+            });
+            expect(a).toEqual('abc');
+        });
+        
+        it('should pass all values from an object', function() {
+            var b = {
+                'one': 'a',
+                'two': 'b',
+                'three': 'c'
+            };
+            var a = '';
+            each(b, function(value) {
+                a += value;
+            });
+            expect(a).toEqual('abc');
+        });
+        
+        it('should pass all keys from an array', function() {
+            var b = ['a', 'b', 'c'];
+            var a = '';
+            each(b, function(value, key) {
+                a += key;
+            });
+            expect(a).toEqual('012');
+        });
+        
+        it('should pass all keys from an object (not always the same order)', function() {
+            var b = {
+                'x': 'a',
+                'y': 'b',
+                'z': 'c'
+            };
+            var a = '';
+            each(b, function(value, key) {
+                a += key;
+            });
+            expect(a).toContain('x');
+            expect(a).toContain('y');
+            expect(a).toContain('z');
+        });
+    });
+});
