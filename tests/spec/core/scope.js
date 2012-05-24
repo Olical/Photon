c279e9683ecf2274daadba8b804ab05c6239ca95
Oliver Caldwell <olliec87@gmail.com>
2012-05-24 21:51:11 +0100

    Added a test for setting the scope.

---
+++ tests/spec/core/scope.js
@@ -0,0 +1,17 @@
+require(['photon/core/scope'], function(scope) {
+    describe('photon/core/scope', function() {
+        it('should set the scope of a function', function() {
+            function fn() {
+                expect(this.foo).toBeUndefined();
+            }
+            function fn2() {
+                expect(this.foo).toEqual(true);
+            }
+            var fnScoped = scope(fn2, {
+                foo: true
+            });
+            fn();
+            fnScoped();
+        });
+    });
+});
