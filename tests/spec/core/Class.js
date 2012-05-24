a86d6759bbb6971ee8c63cb07cb0510d6c521c0c
Oliver Caldwell <olliec87@gmail.com>
2012-05-24 22:21:17 +0100

    Added some basic class testing. Also tests very simple extension.

--- tests/spec/core/Class.js
+++ tests/spec/core/Class.js
@@ -1,5 +1,31 @@
 require(['photon/core/Class'], function(Class) {
     describe('photon/core/Class', function() {
+        it('should create an instance', function() {
+            var a = new Class();
+            expect(a).toBeTruthy();
+        });
         
+        it('should allow methods', function() {
+            var Test = new Class();
+            Test.prototype.foo = function() {
+                return true;
+            };
+            var a = new Test();
+            expect(a.foo()).toEqual(true);
+        });
+        
+        it('should allow extension', function() {
+            var Test = new Class();
+            Test.prototype.foo = function() {
+                return true;
+            };
+            var Test2 = new Class(Test);
+            Test2.prototype.bar = function() {
+                return false;
+            };
+            var a = new Test2();
+            expect(a.foo()).toEqual(true);
+            expect(a.bar()).toEqual(false);
+        });
     });
 });
