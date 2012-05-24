f009da1d3a3dff3bcd1022d860d711aea9b98d2b
Oliver Caldwell <olliec87@gmail.com>
2012-05-24 22:25:05 +0100

    Added a lot more inheritance testing.

--- tests/spec/core/Class.js
+++ tests/spec/core/Class.js
@@ -19,13 +19,27 @@ require(['photon/core/Class'], function(Class) {
             Test.prototype.foo = function() {
                 return true;
             };
+            
             var Test2 = new Class(Test);
             Test2.prototype.bar = function() {
                 return false;
             };
-            var a = new Test2();
+            
+            var Test3 = new Class();
+            Test3.prototype.baz = function() {
+                return 'javascript';
+            };
+            
+            var Test4 = new Class(Test2, Test3);
+            Test4.prototype.boo = function() {
+                return 'test';
+            };
+            
+            var a = new Test4();
             expect(a.foo()).toEqual(true);
             expect(a.bar()).toEqual(false);
+            expect(a.baz()).toEqual('javascript');
+            expect(a.boo()).toEqual('test');
         });
     });
 });
