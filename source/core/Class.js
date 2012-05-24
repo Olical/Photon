224c606a5012c741e81f4567bc8a3e5113d6ef38
Oliver Caldwell <olliec87@gmail.com>
2012-05-24 20:13:38 +0100

    Made a copy of the inherited methods into _super

--- source/core/Class.js
+++ source/core/Class.js
@@ -9,19 +9,28 @@ define(['./merge', './each'], function(merge, each) {
             Each parameter corresponds to another class to inherit from.
     */
     function Class() {
-        // Initialise the new class
+        // Initialise variables
+        var cl = null,
+            inherits = {};
+        
+        // Start the new class
         // This will call the constructor when initialized
-        var cl = function() {
+        cl = function() {
             // Make sure we actually have a construct method
             if(this.hasOwnProperty('construct')) {
                 this.construct.apply(this, arguments);
             }
         };
         
-        // Inherit from all passed classes
+        // Merge all passed classes into one object
         each(arguments, function(value, key) {
-            cl.prototype = merge(cl.prototype, value.prototype);
+            inherits = merge(cl.prototype, value.prototype);
         });
+        
+        // Replace the prototype with the inherited methods
+        // Also clone it into _super, this will allow the super function to work
+        cl.prototype = inherits;
+        cl._super = clone(inherits);
         
         // Return the finished class
         // You can now add your own methods
