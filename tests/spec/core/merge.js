54a7f8fd3f1001728d452f8cae1867edc7e33963
Oliver Caldwell <olliec87@gmail.com>
2012-05-24 21:48:02 +0100

    Added tests for merge

---
+++ tests/spec/core/merge.js
@@ -0,0 +1,48 @@
+require(['photon/core/merge'], function(merge) {
+    describe('photon/core/merge', function() {
+        it('should merge two simple objects', function() {
+            var c = {
+                one: 'foo'
+            };
+            var b = {
+                two: 'bar'
+            };
+            var a = merge(b, c);
+            expect(a.one).toEqual('foo');
+            expect(a.two).toEqual('bar');
+            expect(c.two).toBeUndefined();
+            expect(b.one).toBeUndefined();
+        });
+        
+        it('should merge two deeper objects', function() {
+            var c = {
+                one: {
+                    one: 'foo'
+                }
+            };
+            var b = {
+                one: {
+                    two: 'bar'
+                }
+            };
+            var a = merge(b, c);
+            expect(a.one.one).toEqual('foo');
+            expect(a.one.two).toEqual('bar');
+            expect(c.one.two).toBeUndefined();
+            expect(b.one.one).toBeUndefined();
+        });
+        
+        it('should override arrays', function() {
+            var c = {
+                one: ['a', 'b']
+            };
+            var b = {
+                one: ['1', '2']
+            };
+            var a = merge(b, c);
+            expect(a.one[0]).toEqual('a');
+            expect(a.one[1]).toEqual('b');
+            expect(a.one[2]).toBeUndefined();
+        });
+    });
+});
