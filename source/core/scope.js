8de6c50a5c9df643bc5304c213e410f5f0d344e2
Oliver Caldwell <olliec87@gmail.com>
2012-05-26 18:05:10 +0100

    Documented scope

--- source/core/scope.js
+++ source/core/scope.js
@@ -2,7 +2,25 @@ define(function() {
     /*
         Function: scope
         
-        Returns the function wrapped in another function that calls with the correct scope.
+        Returns the function wrapped in another function that calls with the correct scope. This means that if you have to call a function with a set scope you can create a copy of that function with your specified scope.
+
+        (start code)
+        // Create a basic function
+        function noScope() {
+            return this.foo;
+        }
+
+        // Create a scoped version
+        var withScope = scope(noScope, {
+            foo: true
+        });
+
+        // The default will be undefined
+        noScope(); // undefined
+
+        // But the one with the specified scope will be true
+        withScope(); // true
+        (end)
         
         Parameters:
         
