e61d7c384aa21986739495fb53e5dea22aa81dcb
Oliver Caldwell <olliec87@gmail.com>
2012-05-26 17:35:04 +0100

    Improved meta and loaded it into the docs

--- source/meta.js
+++ source/meta.js
@@ -1,9 +1,12 @@
 /*
-    Photon
-    https://github.com/Wolfy87/Photon
+    File: Meta
 
-    Oliver Caldwell (http://oli.me.uk)
-    Creative Commons Attribution 3.0 Unported License (http://creativecommons.org/licenses/by/3.0/)
+    Photon:
+
+        Version - 1.0.0
+        Repository - https://github.com/Wolfy87/Photon
+        Author - Oliver Caldwell (http://oli.me.uk)
+        License -  Creative Commons Attribution 3.0 Unported License (http://creativecommons.org/licenses/by/3.0/)
 */
 
 define({
