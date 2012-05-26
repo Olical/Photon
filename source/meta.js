fb43a3c27f0d877c5698e614aeeb4e677a1e3242
Oliver Caldwell <olliec87@gmail.com>
2012-05-26 15:24:30 +0100

    Added a basic meta file with version, licence and author information.

---
+++ source/meta.js
@@ -0,0 +1,11 @@
+/*
+    Photon
+    https://github.com/Wolfy87/Photon
+
+    Oliver Caldwell (http://oli.me.uk)
+    Creative Commons Attribution 3.0 Unported License (http://creativecommons.org/licenses/by/3.0/)
+*/
+
+define({
+    version: '1.0.0'
+});
