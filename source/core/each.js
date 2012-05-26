652ae2e53c01cb574393d311ffa34666ee2ae330
Oliver Caldwell <olliec87@gmail.com>
2012-05-26 12:42:38 +0100

    Each now allows you to check if something is iterable.

--- source/core/each.js
+++ source/core/each.js
@@ -5,17 +5,27 @@ define([
         Function: each
         
         Loops over the passed object, string or array and passes each value and key to the callback.
+        If you only pass a list and no callback then it will return true or false depending on whether the list is iterable.
         
         Parameters:
         
             list - Object, string or array to loop over.
             callback - Function for the value and key to be passed to. For an array the key would be the index.
+
+        Returns:
+
+            If you only pass a list then true or false depending on whether it is iterable or not.
     */
     function each(list, callback) {
         // Initialise variables
         var key = null,
             listType = type(list);
         
+        // If the item is undefined then check if the list is indexable
+        if(type(callback) === 'undefined') {
+            return (listType === 'string' || listType === 'array' || listType === 'arguments' || listType === 'object');
+        }
+
         // Loop over the list in different ways depending on what it is
         if(listType === 'object') {
             for(key in list) {
