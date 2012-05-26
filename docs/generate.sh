da6946988d423f107aec7689ce9d2df1d14d8098
Oliver Caldwell <olliec87@gmail.com>
2012-05-26 18:12:56 +0100

    Moved the doc generation to the makefile.

--- docs/generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-perl ${DIR}/NaturalDocs/NaturalDocs -i ${DIR}/../source -o HTML ${DIR}/html -p ${DIR}/project
---
+++ makefile
@@ -0,0 +1,2 @@
+documentation:
+	perl ./docs/NaturalDocs/NaturalDocs -i ./source -o HTML ./docs/html -p ./docs/project
