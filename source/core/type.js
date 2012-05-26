c016a75f872e343cbe725e00bf6385f0107af240
Oliver Caldwell <olliec87@gmail.com>
2012-05-26 18:08:37 +0100

    Documented type

--- source/core/type.js
+++ source/core/type.js
@@ -2,7 +2,29 @@ define(function() {
     /*
         Function: type
         
-        Returns the real type of the variable.
+        Returns the real type of the variable. It is basically an improved version of the typeof statement.
+
+        (start code)
+        var arr = [1, 2, 3];
+        
+        // The default JavaScript typeof is silly
+        typeof arr; // object
+
+        // This method is better
+        type(arr); // array
+        (end)
+
+        Be careful though. This method will also pick up argument arrays separate to arrays.
+
+        (start code)
+        function test(arr) {
+            type(arguments); // arguments
+            type(arr); // array
+        }
+        test([]);
+        (end)
+
+        So please remember to check for both.
         
         Parameters:
         
