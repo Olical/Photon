1a5c3ff05b841034786392fa619f56ac6914b3b2
Oliver Caldwell <olliec87@gmail.com>
2012-05-26 17:44:18 +0100

    Improved each docs

--- source/core/each.js
+++ source/core/each.js
@@ -5,10 +5,28 @@ define([
         Function: each
         
         Loops over the passed object, string or array and passes each value and key to the callback.
-        If you only pass a list and no callback then it will return true or false depending on whether the list is iterable.
-        A string is actually iterable, as in you can pass it to each. But the iterable check will deny strings.
-        This is because iterating over strings is so rare and causes a lot of problems in internal loops.
-        The iterable check should be used to see if the item can contains other items.
+        
+        If you only pass a list and no callback then it will return true or false depending on whether the list is iterable. A string is actually iterable, as in you can pass it to each. But the iterable check will deny strings.
+        
+        This is because iterating over strings is so rare and causes a lot of problems in internal loops. The iterable check should be used to see if the item can contains other items.
+
+        (start code)
+        // Get some example data
+        var users = getUsers();
+
+        // Lets pretend it contained something like this:
+        // {
+        //     'Joe Blogs': {
+        //         age: 20
+        //     }
+        // }
+        
+        // Loop over the data
+        each(users, function(value, key) {
+            // Do stuff with it
+            console.log(key + ' is ' + value.age + ' years old.');
+        });
+        (end)
         
         Parameters:
         
