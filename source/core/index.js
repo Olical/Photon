8ac894c48b8f178ce1240bb46b2cbed508d320e6
Oliver Caldwell <olliec87@gmail.com>
2012-05-26 12:09:48 +0100

    Optimised the index function. Now uses native indexOf when available.

--- source/core/index.js
+++ source/core/index.js
@@ -18,29 +18,27 @@ define([
             The location of the item in the list or -1 if not found.
     */
     function index(list, item) {
-        // Get the types and initialise the index count
-        // Used for finding within an array
-        var listType = type(list),
-            itemType = type(item),
-            i = -1;
+        // Initialise the index count
+        // Used for finding without the native indexOf
+        var i = -1;
         
-        // If the list and item are strings then perform an indexOf
-        if(listType === itemType && listType === 'string') {
+        // Use the native indexOf if available
+        if(type(list.indexOf) === 'function') {
             return list.indexOf(item);
         }
-        else if(listType === 'array') {
-            // It is an array, loop over searching for the item
-            each(list, function(value, index) {
-                // If the value matches the item and the current index is -1
-                // Store the new index
-                if(value === item && i === -1) {
-                    i = index;
-                }
-            });
-            
-            // Return the index
-            return i;
-        }
+        
+        // If we reach here then native indexOf does not exists
+        // Instead to a manual one
+        each(list, function(value, index) {
+            // If the value matches the item and the current index is -1
+            // Store the new index
+            if(value === item && i === -1) {
+                i = index;
+            }
+        });
+        
+        // Return the index
+        return i;
     }
     
     return index;
