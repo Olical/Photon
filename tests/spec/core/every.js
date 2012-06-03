1b663746bf44162ca6b248291610bb2742512f49
Oliver Caldwell <olliec87@gmail.com>
2012-06-03 14:00:04 +0100

    Added the every method and tests.

---
+++ tests/spec/core/every.js
@@ -0,0 +1,33 @@
+define(['photon/core/every'], function(every) {
+    function checker(value, key) {
+        if(value === 'foo' || value === 'bar') {
+            return true;
+        }
+
+        return false;
+    }
+
+    describe('photon/core/every', function() {
+        it('should return true if all are correct', function() {
+            var good = [
+                'foo',
+                'bar',
+                'foo',
+                'bar'
+            ];
+
+            expect(every(good, checker)).toEqual(true);
+        });
+
+        it('should return false if all are wrong', function() {
+            var bad = [
+                'foo',
+                'bar',
+                'baz',
+                'bar'
+            ];
+
+            expect(every(bad, checker)).toEqual(false);
+        });
+    });
+});
