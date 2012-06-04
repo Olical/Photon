f8af65dd36d156d8ace579e3a7aae0b7a54ed2ef
Oliver Caldwell <olliec87@gmail.com>
2012-06-04 13:56:49 +0100

    Improved the naming again. Now called parentClass.

--- source/core/Class.js
+++ source/core/Class.js
@@ -33,7 +33,7 @@ define([
 
         As you can see, all methods are added via the prototype. This keeps with JavaScripts prototypical inheritance.
 
-        If you need to check what class your instance came from you can use the class attribute.
+        If you need to check what class your instance came from you can use the parentClass attribute.
 
         (start code)
         var Foo = new Class();
@@ -42,7 +42,7 @@ define([
         };
 
         var test = new Foo();
-        if(test.instanceOf === Foo) {
+        if(test.parentClass === Foo) {
             console.log('Test is an instance of foo.');
         }
         (end)
@@ -78,7 +78,7 @@ define([
             // Set the instanceOf attribute
             // This contains a copy of the constructor class
             // Can be used to tell what something is
-            this.instanceOf = cl;
+            this.parentClass = cl;
         };
         
         // Merge all passed classes into one object
