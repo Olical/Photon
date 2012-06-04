49c536d3077a3797b2bccc66eee2cf197f51ff1f
Oliver Caldwell <olliec87@gmail.com>
2012-06-04 14:10:03 +0100

    Added more to the Element constructor. Actually creates a new element now.

--- source/dom/Element.js
+++ source/dom/Element.js
@@ -1,6 +1,7 @@
 define([
-    '../core/Class'
-], function() {
+    '../core/Class',
+    '../core/type'
+], function(Class, type) {
     /*
         Class: Element
         
@@ -11,10 +12,33 @@ define([
     /*
         Function: construct
 
-        Called when you initialise a new Element.
-    */
-    Element.prototype.construct = function() {
+        Called when you initialise a new Element. It will adopt the passed native DOM element as well as any passed instance of this class.
 
+        If you pass a string then a new element will be created using the string as it's tag name.
+    */
+    Element.prototype.construct = function(el) {
+        // Check what el is
+        if(type(el) === 'string') {
+            // If it is a string then create a new element
+            this._element = document.createElement(el);
+        }
+        else {
+            // It is not a string, must be an existing element
+            // If the element is an instance of the element class then adopt it's element
+            // Otherwise adopt el as it is
+
+            // TODO: Check that the element is a native DOM element before adopting it - It is assumed at the moment
+            
+            if(el.parentClass === Element) {
+                // It is, adopt it
+                this._element = el._element;
+            }
+            else {
+                // Otherwise it is a native DOM element
+                // Adopt it
+                this._element = el;
+            }
+        }
     };
 
     return Element;
