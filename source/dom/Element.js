1e06e61e87d29e48c159999b766785c48aac4864
Oliver Caldwell <olliec87@gmail.com>
2012-06-04 13:04:50 +0100

    Added the basic Element class.

---
+++ source/dom/Element.js
@@ -0,0 +1,12 @@
+define([
+    '../core/Class'
+], function() {
+    /*
+        Class: Element
+        
+        Base element class. It is a container for native DOM elements that provides a normalised API for the DOM.
+    */
+    var Element = new Class();
+
+    return Element;
+});
