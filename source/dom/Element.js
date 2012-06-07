c276a43228afed7c72dff0633d86ad7e8a3a32f7
Oliver Caldwell <olliec87@gmail.com>
2012-06-07 21:32:35 +0100

    Added the has method.

--- source/dom/Element.js
+++ source/dom/Element.js
@@ -88,6 +88,29 @@ define([
     };
 
     /*
+        Function: has
+        
+        Checks if the element has the specified attribute. If no attribute is passed then it will check if the element has any attributes.
+
+        Parameters:
+
+            key - The name of the attribute to check for. Optional. If not passed then it will check for any attributes at all.
+
+        Returns:
+
+            Either true or false. If you pass a key then it will return true when found. If you do not pass a key then it will return true if the element has any attributes.
+    */
+    Element.prototype.has = function(key) {
+        // If there is a key then check for it
+        if(key) {
+            return this.element.hasAttribute(key);
+        }
+
+        // Otherwise check if there are any attributes at all
+        return this.element.hasAttributes();
+    };
+
+    /*
         Function: getParent
         
         Retrieves the parent of the current element.
