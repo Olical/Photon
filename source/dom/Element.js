b214abdda0cb7440ba9d4708ff051f2d0d978e73
Oliver Caldwell <olliec87@gmail.com>
2012-06-10 17:49:50 +0100

    Fixed the hasAttribute method in IE. Checking if an empty string was returned. Not null.

--- source/dom/Element.js
+++ source/dom/Element.js
@@ -157,7 +157,7 @@ define([
         }
         else {
             // Otherwise use getAttribute with a null check
-            res = (this.element.getAttribute('key') !== null);
+            res = (this.element.getAttribute('key') !== '');
         }
 
         return res;
