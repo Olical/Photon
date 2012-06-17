e8f7a63d95d1fb1205a3dc5745f36c411d1e539c
Oliver Caldwell <olliec87@gmail.com>
2012-06-17 11:36:27 +0100

    Added more comments to the element list class

--- source/dom/ElementList.js
+++ source/dom/ElementList.js
@@ -5,7 +5,26 @@ define([
     /*
         Class: ElementList
 
-        A wrapper array to contain a list of elements. It creates a native array and then inserts extra methods to be used, so <addClass> will be run on all contained elements.
+        A wrapper to contain an array of elements. It maps methods from the <Element> class to a version that will be run on all items like this.
+
+        (start code)
+        var list = new ElementList([el, ...]);
+        list.addClass('foo'); // foo added to all elements
+        (end)
+
+        If you wish to edit the actual array inside it to add or remove elements then you must access the `items` attribute.
+
+        (start code)
+        // Need to loop over all? Use list.items.
+        var list = new ElementList([el, ...]);
+
+        each(list.items, function() {
+            // ...
+        });
+
+        // All array methods are found here
+        list.items.push(new Element('div'));
+        (end)
 
         All elements within this array are instances of <Element>.
 
@@ -15,6 +34,19 @@ define([
             - <Element>
     */
     var ElementList = new Class();
+
+    /*
+        Function: construct
+
+        Initialises the storage array and places any passed elements inside it.
+
+        Parameters:
+
+            els - An array of native DOM elements or instances of <Element>. You do not have to pass it. You can add more later with: `yourList.items.push(...)`.
+    */
+    ElementList.prototype.construct = function(els) {
+
+    };
 
     return ElementList;
 });
