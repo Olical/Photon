d2fc3dca773bfb305567fa4ce8e2c41cf2b1440c
Oliver Caldwell <olliec87@gmail.com>
2012-06-23 21:01:51 +0100

    Added event firing tests

--- tests/spec/manage/Events.js
+++ tests/spec/manage/Events.js
@@ -1,5 +1,10 @@
 define(['photon/manage/Events', 'photon/core/Class'], function(Events, Class) {
     describe('photon/manage/Events', function() {
+        it('', function() {
+            var Test = Class(Events);
+            var a = new Test();
+        });
+
         it('get events', function() {
             var Test = Class(Events);
             var a = new Test();
@@ -8,8 +13,96 @@ define(['photon/manage/Events', 'photon/core/Class'], function(Events, Class) {
             expect(a.getEvents('foo')).toEqual([]);
         });
 
-        // it('register events', function() {
+        it('add events', function() {
+            var Test = Class(Events);
+            var a = new Test();
+            function t1() { return true; }
+            function t2() { return true; }
 
-        // });
+            expect(a.getEvents('foo')).toEqual([]);
+            a.addEvent('foo', t1);
+            expect(a.getEvents('foo')).toEqual([t1]);
+
+            a.addEvent({
+                foo: t2,
+                bar: t1
+            });
+
+            expect(a.getEvents('foo')).toEqual([t1, t2]);
+            expect(a.getEvents('bar')).toEqual([t1]);
+        });
+
+        it('remove events', function() {
+            var Test = Class(Events);
+            var a = new Test();
+            function t1() { return true; }
+            function t2() { return true; }
+
+            expect(a.getEvents('foo')).toEqual([]);
+            a.addEvent('foo', t1);
+            expect(a.getEvents('foo')).toEqual([t1]);
+
+            a.addEvent({
+                foo: t2,
+                bar: t1
+            });
+
+            expect(a.getEvents('foo')).toEqual([t1, t2]);
+            expect(a.getEvents('bar')).toEqual([t1]);
+
+            a.removeEvent('bar');
+            expect(a.getEvents('bar')).toEqual([]);
+
+            a.removeEvent('foo', t2);
+            expect(a.getEvents('foo')).toEqual([t1]);
+
+            a.removeEvent();
+            expect(a.getEvents('foo')).toEqual([]);
+
+            a.addEvent('foo', t1);
+            a.addEvent({
+                foo: t2,
+                bar: t1
+            });
+
+            a.removeEvent({
+                foo: t1,
+                bar: t1
+            });
+
+            expect(a.getEvents('foo')).toEqual([t2]);
+            expect(a.getEvents('bar')).toEqual([]);
+
+            a.removeEvent();
+
+            expect(a.getEvents()).toEqual({});
+            expect(a.getEvents('foo')).toEqual([]);
+            expect(a.getEvents('bar')).toEqual([]);
+        });
+
+        it('fire events', function() {
+            var Test = Class(Events);
+            var a = new Test();
+
+            var res1 = null;
+            var res2 = null;
+
+            function t1() {
+                res1 = arguments;
+            }
+
+            function t2() {
+                res2 = arguments;
+            }
+
+            a.addEvent('foo', t1);
+            a.addEvent('bar', t2);
+
+            a.fireEvent('foo');
+            a.fireEvent('bar', [true, false]);
+
+            expect(res1).toEqual([]);
+            expect(res2).toEqual([true, false]);
+        });
     });
 });
