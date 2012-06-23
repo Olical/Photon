2dd8eef70e7501db72baa5e14fb2e6ff90359fd6
Oliver Caldwell <olliec87@gmail.com>
2012-06-23 19:11:11 +0100

    Added the basic Events file.

---
+++ source/manage/Events.js
@@ -0,0 +1,16 @@
+define([
+    '../core/Class'
+], function(Class) {
+    /*
+        Class: Events
+
+        An event managing class that can be extended by your classes.
+
+        Requires:
+
+            - <Class>
+    */
+    var Events = Class();
+
+    return Events;
+});
