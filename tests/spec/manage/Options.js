1f2396a2ea8001b7a3c3942c069c387ca2b0aec5
Oliver Caldwell <olliec87@gmail.com>
2012-06-24 11:58:18 +0100

    Added a test of adding events through options.

--- tests/spec/manage/Options.js
+++ tests/spec/manage/Options.js
@@ -1,4 +1,4 @@
-define(['photon/manage/Options', 'photon/core/Class'], function(Options, Class) {
+define(['photon/manage/Options', 'photon/core/Class', 'photon/manage/Events'], function(Options, Class, Events) {
     describe('photon/manage/Options', function() {
         it('setting of options', function() {
             var Test = Class(Options);
@@ -26,6 +26,25 @@ define(['photon/manage/Options', 'photon/core/Class'], function(Options, Class) 
                 foo: true
             });
             expect(t.options).toEqual({ foo: true, bar: false });
+        });
+
+        it('add events with options', function() {
+            var Test = Class(Options, Events);
+            Test.fn.construct = function(options) {
+                this.setOptions(options);
+            };
+
+            var res = false;
+            var t = new Test({
+                events: {
+                    foo: function() {
+                        res = true;
+                    }
+                }
+            });
+            t.fireEvent('foo');
+
+            expect(res).toEqual(true);
         });
     });
 });
