f38b4cca5e56a30d04e2978f0d9465c4686eda66
Oliver Caldwell <olliec87@gmail.com>
2012-06-28 21:43:56 +0100

    Keys now uses Object.keys if found.

--- source/core/keys.js
+++ source/core/keys.js
@@ -1,10 +1,13 @@
 define([
-    './each'
-], function(each) {
+    './each',
+    './type'
+], function(each, type) {
     /*
         Function: keys
         
         Returns an array of keys from the passed array or object.
+
+        The keys function will leverage <Object.keys at https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object/keys> if it is present in your browser.
 
         (start code)
         var someObj = {
@@ -26,8 +29,15 @@ define([
         Requires:
 
             - <each>
+            - <type>
     */
     function keys(list) {
+        // Use the native method
+        if(type(list) === 'object' && type(Object.keys) === 'function') {
+            // Return the result of the native method
+            return Object.keys(list);
+        }
+
         // Build the list of keys and return them
         var keyList = [];
 
